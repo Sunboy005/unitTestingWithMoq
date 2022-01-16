87e0e0d61df9f1c4765660068c6abd66e93b0d51
Amos Alagbe <sunboy005@gmail.com>
2022-01-16 23:58:24 +0100

    NewUpdate

---
+++ StringCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace TestDrivenDesign
+{
+    public class stringCalculator
+    {
+        public int AddStrings(string strSeparatedWithComma)
+        {
+            var strSplit = new List<string>();
+            var result = 0;
+
+            if (strSeparatedWithComma == "")
+            {
+                strSplit.Add("0");
+            }
+            else
+            {
+                var strSplitted = strSeparatedWithComma.Split(',');
+                foreach (var item in strSplitted)
+                {
+                    strSplit.Add(item);
+                }
+            }
+            for (int i = 0; i < strSplit.Count; i++)
+            {
+                result += int.Parse(strSplit[i]);
+            }
+
+            return result;
+        }
+    }
+}
